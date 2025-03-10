752be6d03db4bf94b7b0a70743f23a5df5c552d5
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-10 14:48:56 +0000

    add authgate page

---
+++ lib/auth/auth_gate.dart
@@ -0,0 +1,32 @@
+import 'package:authentification/pages/login_page.dart';
+import 'package:authentification/pages/profile_page.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Supabase.instance.client.auth.onAuthStateChange,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        final session = snapshot.hasData ? snapshot.data!.session : null;
+
+        if (session != null) {
+          return ProfilePage();
+        } else {
+          return LoginPage();
+        }
+      },
+    );
+  }
+}
