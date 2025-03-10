a00ec856303ff0dd453bf086ca9d231de0ec7aba
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-10 15:09:46 +0000

    add profile page

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,32 @@
+import 'package:authentification/auth/auht_service.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final authService = AuthService();
+  void logout() async {
+    await authService.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentEmail = authService.getCurrentUserEmail();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Welcome <3 .."),
+        ),
+        actions: [IconButton(onPressed: logout, icon: Icon(Icons.logout))],
+      ),
+      body: Center(
+        child: Text("$currentEmail"),
+      ),
+    );
+  }
+}
