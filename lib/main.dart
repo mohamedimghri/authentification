b0519e8ff85c4497bcb3056b7b17e13dd50d9bcd
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-10 14:47:24 +0000

    supabase initilized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:authentification/auth/auth_gate.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async{
+
+  await Supabase.initialize(
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzcXFpdWVkZXJwYnJhdHd6YWFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MTEwOTksImV4cCI6MjA1NzE4NzA5OX0.RhTewPEmEf49FYxReJcyGvkCIZKFAJN4EHMyR1bIHxI",
+    url: "https://fsqqiuederpbratwzaac.supabase.co",
+    );
+
   runApp(const MyApp());
 }
 
@@ -11,9 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        
-      ),
+      home: AuthGate(),
     );
   }
 }
